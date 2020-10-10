0fabac00558abef7092333ee02738095be3638d5
runner <runner@Mac-1602325311626.local>
2020-10-10 10:29:49 +0000

    update: SDWebImageSwiftUI

---
+++ SDWebImageSwiftUI/CHANGELOG.md
@@ -0,0 +1,75 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.5.0] - 2020-06-01
+### Added
+- Add the convenient API support to use SwiftUI transition with ease-in-out duration #116
+- Update the Travis-CI to use Catalina and enable macOS test case #98
+
+## [1.4.0] - 2020-05-07
+### Added
+- Add the same overload method for onSuccess API, which introduce the image data arg. Keep the source code compatibility #109
+- Add the support for image data observable on ImageManager #107
+
+## [1.3.4] - 2020-04-30
+### Fixed
+- Revert the changes to prefetch the image url from memory cache #106
+
+## [1.3.3] - 2020-04-15
+### Fixed
+- Try to solve the SwiftUI bug of rendering EXIF UIImage in WebImage, as well as vector images #102
+- Now `WebImage` will render the vector images as bitmap version even if you don't provide `.thumbnailPixelSize`. To render real vector images, use `AnimatedImage` instead.
+
+## [1.3.2] - 2020-04-14
+### Added
+- Automatically import SDWebImage when user write import SDWebImageSwiftUI #100
+
+## [1.3.1] - 2020-04-10
+### Fixed
+- Fix Carthage support. Do not embed SDWebImage.framework in SDWebImageSwiftUI.framework #97. Thanks @jonkan
+
+## [1.3.0] - 2020-04-05
+### Added
+- Supports the `placeholder` View Builder API for `AnimatedImage` #94
+
+### Changed
+- Upgrade the dependency of SDWebImage 5.7.0 #93
+
+## [1.2.1] - 2020-04-01
+### Fixed
+- Fix the issue when using `WebImage` with some transition like scaleEffect, each time the new state update will cause unused image fetching #92
+
+## [1.2.0] - 2020-03-29
+### Added
+- Supports the `delayPlaceholder` for WebImage #91
+- `AnimatedImage` little patch - UIKit/AppKit animated image now applied for `resizingMode` #89
+
+### Fixed
+- Fix the issue when dealloc `AnimatedImage`'s native View, the window does not exist and cause Crash #90
+
+## [1.1.0] - 2020-03-24
+### Added
+- `ImageManager` now public. Which allows advanced usage for custom View type. Use `@ObservedObject` to bind the manager with your own View and update the image.
+
+## [1.0.0] - 2020-03-03
+### Added
+- `WebImage` now supports animation, use `isAnimating` binding value on init methods.
+- `WebImage` now supports the detailed animation control options, like `customLoopCount`, `pausable`, `purgeable`, `playbackRate`.
+- `AnimatedImage` now supports the indicator with `ViewModifier` as `WebImage`.
+- `IndicatorViewModifier` now public.
+- `IndicatorReportable` now public.
+
+### Changed
+- Indicator's `progress` type now changed from `CGFloat` to `Double`.
+- `WebImage.aniamted(_:)` now becomes the `WebImage.init(url:options:context:isAnimating:)` Binding arg, you can use the Binding to control animations as well.
+- `AnimatedImage.playBackRate` now becomes `AnimatedImage.playbackRate`
+- `AnimatedImage.customLoopCount` now is `UInt` instead of `Int`.
+- `AnimatedImage.resizable` modifier now matches the SwiftUI behavior, you must call it or the size will be fixed to image pixel size.
+
+### Removed
+- Removed all the description about 0.x version behavior in README.md.
---
+++ SDWebImageSwiftUI/Example/SDWebImageSwiftUIDemo/ContentView.swift
@@ -0,0 +1,223 @@
+/*
+ * This file is part of the SDWebImage package.
+ * (c) DreamPiggy <lizhuoli1126@126.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+class UserSettings: ObservableObject {
+    // Some environment configuration
+    #if os(tvOS)
+    @Published var editMode: EditMode = .inactive
+    @Published var zoomed: Bool = false
+    #endif
+}
+
+#if os(watchOS)
+// watchOS does not provide built-in indicator, use Espera's custom indicator
+struct ActivityIndicator : View {
+    @Binding var isAnimating: Bool
+    var body: some View {
+        if isAnimating {
+            return AnyView(LoadingFlowerView()
+                .frame(width: 30, height: 30))
+        } else {
+            return AnyView(EmptyView()
+                .frame(width: 30, height: 30))
+        }
+    }
+}
+
+struct ProgressIndicator : View {
+    @Binding var isAnimating: Bool
+    @Binding var progress: Double
+    var body: some View {
+        if isAnimating {
+            return AnyView(StretchProgressView(progress: $progress)
+                .frame(width: 140, height: 10))
+        } else {
+            return AnyView(EmptyView()
+                .frame(width: 140, height: 10))
+        }
+    }
+}
+
+extension Indicator where T == ActivityIndicator {
+    /// Activity Indicator
+    static var activity: Indicator {
+        Indicator { isAnimating, _ in
+            ActivityIndicator(isAnimating: isAnimating)
+        }
+    }
+}
+
+extension Indicator where T == ProgressIndicator {
+    /// Progress Indicator
+    static var progress: Indicator {
+        Indicator { isAnimating, progress in
+            ProgressIndicator(isAnimating: isAnimating, progress: progress)
+        }
+    }
+}
+#endif
+
+struct ContentView: View {
+    @State var imageURLs = [
+    "http://assets.sbnation.com/assets/2512203/dogflops.gif",
+    "https://raw.githubusercontent.com/liyong03/YLGIFImage/master/YLGIFImageDemo/YLGIFImageDemo/joy.gif",
+    "http://apng.onevcat.com/assets/elephant.png",
+    "http://www.ioncannon.net/wp-content/uploads/2011/06/test2.webp",
+    "http://www.ioncannon.net/wp-content/uploads/2011/06/test9.webp",
+    "http://littlesvr.ca/apng/images/SteamEngine.webp",
+    "http://littlesvr.ca/apng/images/world-cup-2014-42.webp",
+    "https://isparta.github.io/compare-webp/image/gif_webp/webp/2.webp",
+    "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic",
+    "https://nokiatech.github.io/heif/content/image_sequences/starfield_animation.heic",
+    "https://www.sample-videos.com/img/Sample-png-image-1mb.png",
+    "https://nr-platform.s3.amazonaws.com/uploads/platform/published_extension/branding_icon/275/AmazonS3.png",
+    "https://raw.githubusercontent.com/ibireme/YYImage/master/Demo/YYImageDemo/mew_baseline.jpg",
+    "https://via.placeholder.com/200x200.jpg",
+    "https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_5.jpg",
+    "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/w3c.svg",
+    "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/wikimedia.svg",
+    "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/stack_of_photos.pdf",
+    "https://raw.githubusercontent.com/icons8/flat-color-icons/master/pdf/smartphone_tablet.pdf"
+    ]
+    @State var animated: Bool = true // You can change between WebImage/AnimatedImage
+    @EnvironmentObject var settings: UserSettings
+    
+    var body: some View {
+        #if os(iOS)
+        return NavigationView {
+            contentView()
+            .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
+            .navigationBarItems(leading:
+                Button(action: { self.reloadCache() }) {
+                    Text("Reload")
+                }, trailing:
+                Button(action: { self.switchView() }) {
+                    Text("Switch")
+                }
+            )
+        }
+        #endif
+        #if os(tvOS)
+        return NavigationView {
+            contentView()
+            .environment(\EnvironmentValues.editMode, self.$settings.editMode)
+            .navigationBarTitle(animated ? "AnimatedImage" : "WebImage")
+            .navigationBarItems(leading:
+                Button(action: { self.reloadCache() }) {
+                    Text("Reload")
+                }, trailing:
+                Button(action: { self.switchView() }) {
+                    Text("Switch")
+                }
+            )
+        }
+        #endif
+        #if os(macOS)
+        return NavigationView {
+            contentView()
+            .frame(minWidth: 200)
+            .listStyle(SidebarListStyle())
+            .contextMenu {
+                Button(action: { self.reloadCache() }) {
+                    Text("Reload")
+                }
+                Button(action: { self.switchView() }) {
+                    Text("Switch")
+                }
+            }
+        }
+        #endif
+        #if os(watchOS)
+        return contentView()
+            .contextMenu {
+                Button(action: { self.reloadCache() }) {
+                    Text("Reload")
+                }
+                Button(action: { self.switchView() }) {
+                    Text("Switch")
+                }
+            }
+        #endif
+    }
+    
+    func contentView() -> some View {
+        List {
+            ForEach(imageURLs, id: \.self) { url in
+                NavigationLink(destination: DetailView(url: url, animated: self.animated)) {
+                    HStack {
+                        if self.animated {
+                            #if os(macOS) || os(iOS) || os(tvOS)
+                            AnimatedImage(url: URL(string:url))
+                            .onViewUpdate { view, context in
+                            #if os(macOS)
+                                view.toolTip = url
+                            #endif
+                            }
+                            .indicator(SDWebImageActivityIndicator.medium)
+                            /**
+                            .placeholder(UIImage(systemName: "photo"))
+                            */
+                            .transition(.fade)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                            #else
+                            WebImage(url: URL(string:url), isAnimating: self.$animated)
+                            .resizable()
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                            #endif
+                        } else {
+                            WebImage(url: URL(string:url))
+                            .resizable()
+                            /**
+                             .placeholder {
+                                 Image(systemName: "photo")
+                             }
+                             */
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFit()
+                            .frame(width: CGFloat(100), height: CGFloat(100), alignment: .center)
+                        }
+                        Text((url as NSString).lastPathComponent)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            .onDelete { indexSet in
+                indexSet.forEach { index in
+                    self.imageURLs.remove(at: index)
+                }
+            }
+        }
+    }
+    
+    func reloadCache() {
+        SDImageCache.shared.clearMemory()
+        SDImageCache.shared.clearDisk(onCompletion: nil)
+    }
+    
+    func switchView() {
+        SDImageCache.shared.clearMemory()
+        animated.toggle()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
